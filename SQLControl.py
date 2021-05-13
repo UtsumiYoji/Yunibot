71e1ae83c7bfa269c441e3e6bf3ef66c0be92741
Utsumi <yoji.utsumi@hpk.co.jp>
2021-05-13 19:50:22 +0900

    SQLクラスはとりあえず完成?

--- SQLControl.py
+++ SQLControl.py
@@ -1,4 +1,4 @@
-from os import curdir
+from os import curdir, set_inheritable
 import sqlite3
 
 class SQLControl:
@@ -27,8 +27,7 @@ class SQLControl:
             "create table TotsuStatus(\
                 Memberid integer,\
                 RemainTotsu integer default 3,\
-                NowTotsu integer default 0,\
-                )"
+                NowTotsu integer default 0)"
         )
         #30人分登録
         for i in range(30):
@@ -44,20 +43,27 @@ class SQLControl:
                 ReservationBossNo integer,\
                 lap integer,\
                 ReservationComment string,\
-                Done integer default 0,\
-                )"
+                Done integer default 0)"
         )
 
         #持ち越し登録テーブル
         self.cur.execute(
             "create table CarryOver(\
+                CarryOverid integer primary key autoincrement,\
                 Memberid integer,\
                 BossNo integer,\
                 RemainSecond integer,\
                 party string,\
                 comment string,\
-                Done integer default 0,\
-            )"
+                Done integer default 0)"
+        )
+
+        #周回数テーブル
+        self.cur.execute(
+            "create table Laps(Lapid integer primary key autoincrement, lap integer)"
+        )
+        self.cur.execute(
+            "insert into Laps(lap) values(1)"
         )
     
     #メンバーの追加
@@ -92,7 +98,7 @@ class SQLControl:
     #本線開始状態への変更
     def atk(self, Memberid):
         self.cur.execute(
-            "update TotsuStatus set NowTotsu = 1 where Memberid =" + str(id)
+            "update TotsuStatus set NowTotsu = 1 where Memberid =" + str(Memberid)
         )
     
     #本線終了の宣言
@@ -122,7 +128,7 @@ class SQLControl:
     def Reservation(self, Memberid, BossNo, lap, comment):
         self.cur.execute(
             "insert into Reservation(Memberid, ReservationBossNo, lap, ReservationComment) \
-                value("+str(Memberid)+","+str(BossNo)+","+str(lap)+",'"+comment+"')"
+                values("+str(Memberid)+","+str(BossNo)+","+str(lap)+",'"+comment+"')"
         )
     
     #予約の消費(削除)
@@ -138,9 +144,41 @@ class SQLControl:
         )
         return self.cur.fetchall()
     
+    #持ち越しの登録
+    def CarryOver(self, Memberid, BossNo, RemainSecond, party, comment):
+        self.cur.execute(
+            "insert into CarryOver(Memberid, BossNo, RemainSecond, party, comment) \
+                values("+str(Memberid)+","+str(BossNo)+","+str(RemainSecond)+",'"+party+"','"+comment+"')"
+        )
+    
+    #持ち越しの完了
+    def DoneCarryOver(self, CarryOverid):
+        self.cur.execute(
+            "update CarryOver set Done = 1 where CarryOverid =" + str(CarryOverid)
+        )
+
     #未完了持ち越し状況の取得
-    def ReservationStatus(self):
+    def CarryOverStatus(self):
         self.cur.execute(
             "select * from CarryOver where Done = 0"
         )
         return self.cur.fetchall()
+    
+    #周回数の変更
+    def LapChange(self, lap):
+        self.cur.execute(
+            "update Laps set lap = " + str(lap) + " where Lapid = 1"
+        )
+    
+    #周回数の取得
+    def LapStatus(self):
+        self.cur.execute(
+            "select * from Laps"
+        )
+        return self.cur.fetchall()[0][1]
+
+ins = SQLControl()
+TotsuStatus = ins.TotsuStatus()
+
+for i in range(len(TotsuStatus)):
+    print(TotsuStatus[i])
